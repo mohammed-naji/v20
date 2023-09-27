336a5af0814ea148b49560b3297449c57cab2627
Mohammed Naji <m.naji1512@gmail.com>
2023-09-27 15:49:46 +0300

    new routes added

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,47 @@ use Illuminate\Support\Facades\Route;
 
 // use , namespace
 
-Route::get('/', function () {
-    return 'Homepage';
+// Route::get('/', function () {
+//     return 'Homepage';
+// });
+
+// Route::get('/about', function () {
+//     return 'About Us';
+// });
+
+// Route::get('/contact-us', function () {
+//     return 'Contact Us';
+// });
+
+// php namespace resolver
+//
+
+Route::post('/', function() {
+    return 'homepage - post';
 });
 
-Route::get('/about', function () {
+Route::put('/', function() {
+    return 'homepage - put';
+});
+
+Route::get('/', function() {
+    return 'homepage - get';
+});
+
+Route::get('/about', function() {
     return 'About Us';
 });
 
-Route::get('/contact-us', function () {
-    return 'Contact Us';
+// Route::get('user/{name}', function($name) {
+//     return "Welcome " . $name;
+// });
+
+Route::get('user/{name}/{age}', function($name, $age) {
+    return "Welcome " . $name . ", your age is " . $age;
 });
 
-// php namespace resolver
+Route::get('sessions/{course}/{type?}', function($course, $type = 'live-online') {
+    return "Course: $course, Type: $type";
+});
+
 //
